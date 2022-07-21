5e155f0016a8a60f4f4d725f98b24d6e9e3a4aa6
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-07-21 15:55:31 +0300

    Add files via upload

---
+++ Project8_cplusplus.cpp
@@ -0,0 +1,38 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara/78
+//Bu program bir dik üçgenin alanını ve çevresini hesaplamak için yazılmıştır.
+//This program written for calculate perimeter and area of triangle.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double short_side;
+    double long_side;
+    double hipotenüs;
+    double perimeter;
+    double area;
+
+    cout << "Please enter short side :" << endl;
+    cin >> short_side;
+    cout << endl;
+    cout << "Please enter long side:" << endl;
+    cin >> long_side;
+    cout << endl;
+    cout << "Please enter hipotenus :" << endl;
+    cin >> hipotenüs;
+    cout << endl;
+
+    perimeter = short_side + long_side + hipotenüs;
+
+    area = (short_side * long_side) / 2;
+
+    cout <<"Perimeter of triangle :\t " << perimeter << endl;
+
+    cout << "Area of triangle :\t" << area << endl;
+
+    system("PAUSE");
+    return 0;
+
+}
+
